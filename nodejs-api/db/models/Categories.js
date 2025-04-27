f8a7ed9f852e9daabab6f38b48703c64ae8c2ae2
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2025-04-27 18:12:20 +0300

    category endpoints created, enums file created, response file created, error model created

--- nodejs-api/db/models/Categories.js
+++ nodejs-api/db/models/Categories.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const schema = mongoose.Schema(
   {
-    is_Active: { type: Boolean, default: true },
-    created_by: { type: mongoose.SchemaTypes.ObjectId, required: true },
+    is_active: { type: Boolean, default: true },
+    name: { type: String, required: true },
+    created_by: { type: mongoose.SchemaTypes.ObjectId },
   },
   {
     versionKey: false,
